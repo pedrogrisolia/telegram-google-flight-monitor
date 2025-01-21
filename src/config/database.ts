f10a477d3f15b74599899b95009e784222c52845
pedrogrisolia <pedrogrisolia@protonmail.com>
2025-01-21 15:13:43 -0300

    Add PriceHistory entity to database configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,12 +2,13 @@ import { DataSource } from "typeorm";
 import { User } from "../entities/User";
 import { Trip } from "../entities/Trip";
 import { Flight } from "../entities/Flight";
+import { PriceHistory } from "../entities/PriceHistory";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     url: process.env.MYSQL_URL,
     synchronize: true,
-    entities: [User, Trip, Flight],
+    entities: [User, Trip, Flight, PriceHistory],
     ssl: {
         rejectUnauthorized: false
     }
