31255f6d3870bc8e6b0bcad9b3d975e5c819b830
pedrogrisolia <pedrogrisolia@protonmail.com>
2025-05-19 16:44:41 -0300

    Remove Docker-related files and update database configuration to use PostgreSQL with environment variable for database URL.

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,13 +3,16 @@ import { User } from "../entities/User";
 import { Trip } from "../entities/Trip";
 import { Flight } from "../entities/Flight";
 import { PriceHistory } from "../entities/PriceHistory";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    url: process.env.MYSQL_URL,
-    synchronize: true,
-    entities: [User, Trip, Flight, PriceHistory],
-    ssl: {
-        rejectUnauthorized: false
-    }
-}); 
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  entities: [User, Trip, Flight, PriceHistory],
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
